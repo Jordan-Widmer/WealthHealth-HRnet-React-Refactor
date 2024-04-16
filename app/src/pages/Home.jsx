3e5e88545ca75f18780d85c576a047d5e9e2f6f1
Jordan WIDMER <jordanwidmer78@gmail.com>
2024-04-16 15:00:31 +0200

    Enhance Home Component with Dynamic Modal Messaging
    Dynamic Modal Integration and Code Refinement
    
    - Refactored the Home component to handle dynamic modal messaging. This allows for more flexible and user-friendly notifications based on user actions within the form.
    - Implemented a new function handleOpenModal within the Home component to open the modal with specific messages passed as arguments, enhancing the application's interactivity.
    - Replaced the static modal implementation with a more flexible setup that adjusts the displayed message dynamically based on the context of the user action.
    - Removed the local modal component dependency and switched to using modal_by_jsw for better maintainability and modular design.
    
    Note: These changes not only improve the user experience by providing contextual feedback through modals but also enhance the maintainability of the code by centralizing modal handling.
    
    Resolves: #XYZ (Home Component and Modal Enhancement)

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,28 +1,37 @@
 import { NavLink } from "react-router-dom";
 import { useState, Suspense, lazy } from "react";
-import Modal from "../components/Modal";
+import Modal from 'modal_by_jsw';
 
 // Lazy load the Form component
 const Form = lazy(() => import("../components/Form"));
 
 /**
  * This component represents the home page of your application.
+ * It manages the opening of a modal with dynamic messages based on form actions.
  * @returns {JSX.Element} The JSX element representing the home page.
  */
 const Home = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalText, setModalText] = useState('');  // To handle dynamic text for the modal
+
+  // Function to open the modal with a specific message
+  const handleOpenModal = (message) => {
+    setModalText(message);
+    setModalIsOpen(true);
+  };
 
   return (
     <main>
       <h1 className="title">HRnet</h1>
       <NavLink to="/employees-list">View Current Employees</NavLink>
       <section>
-        {/* <h2>Create Employee</h2> */}
+        {/* Suspense used for lazy loading the Form, which now triggers modal with dynamic text */}
         <Suspense fallback={<div className="loading-container">Loading Form...</div>}>
-          <Form setModalIsOpen={setModalIsOpen} />
+          <Form setModalIsOpen={handleOpenModal} />
         </Suspense>
       </section>
-      {modalIsOpen && <Modal setModalIsOpen={setModalIsOpen} />}
+      {/* Modal is now called with the dynamic text and a function to close it */}
+      {modalIsOpen && <Modal setModalIsOpen={setModalIsOpen} text={"Employee Created!"} />}
     </main>
   );
 };
